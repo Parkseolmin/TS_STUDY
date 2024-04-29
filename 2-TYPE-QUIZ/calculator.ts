c143f5b9c836891729db566cb4317fea9a652862
Parkseolmin <qkrtjfals2@naver.com>
2024-04-29 20:31:16 +0900

    TYPE-QUIZ

--- 2-TYPE-QUIZ/calculator.ts
+++ 2-TYPE-QUIZ/calculator.ts
@@ -1,6 +1,24 @@
 /**
  * Let's make a calculator 🧮
  */
+type State = 'add' | 'substract' | 'multiply' | 'divide' | 'remainder';
+function calculate(state: State, num1: number, num2: number) {
+  switch (state) {
+    case 'add':
+      return num1 + num2;
+    case 'substract':
+      return num1 - num2;
+    case 'multiply':
+      return num1 * num2;
+    case 'divide':
+      return num1 / num2;
+    case 'remainder':
+      return num1 % num2;
+    default:
+      throw Error('unknown state: ' + state);
+  }
+}
+
 console.log(calculate('add', 1, 3)); // 4
 console.log(calculate('substract', 3, 1)); // 2
 console.log(calculate('multiply', 4, 2)); // 8
--- 2-TYPE-QUIZ/game.ts
+++ 2-TYPE-QUIZ/game.ts
@@ -1,6 +1,33 @@
 /**
  * Let's make a game 🕹
  */
+type Position = {
+  x: number;
+  y: number;
+};
+
+type Direction = 'up' | 'down' | 'left' | 'right';
+let position: Position = {
+  x: 0,
+  y: 0,
+};
+function move(direction: Direction) {
+  switch (direction) {
+    case 'up':
+      position.y += 1;
+      break;
+    case 'down':
+      position.y -= 1;
+      break;
+    case 'left':
+      position.x -= 1;
+      break;
+    case 'right':
+      position.x += 1;
+      break;
+  }
+}
+
 console.log(position); // { x: 0, y: 0}
 move('up');
 console.log(position); // { x: 0, y: 1}
